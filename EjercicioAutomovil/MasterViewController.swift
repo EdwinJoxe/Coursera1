834b560f3bbd798e62feb4bd763ebfc955d18bd3
Edwin José Gonzalez <edwinjoxe@hotmail.com>
2016-07-02 18:20:04 +0200

    comentario2

--- EjercicioAutomovil/MasterViewController.swift
+++ EjercicioAutomovil/MasterViewController.swift
@@ -1,94 +1,65 @@
-//
-//  MasterViewController.swift
-//  EjercicioAutomovil
-//
-//  Created by Edwin José on 2/07/16.
-//  Copyright © 2016 Edwin José. All rights reserved.
-//
+
 
 import UIKit
 
-class MasterViewController: UITableViewController {
 
-    var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
-    //modif1
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    init(velocidadInicial : Velocidades) {
+        self = .Apagado
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-        self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
-        super.viewWillAppear(animated)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func insertNewObject(_ sender: AnyObject) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        self.tableView.insertRows(at: [indexPath], with: .automatic)
-    }
-
-    // MARK: - Segues
-
-    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
-    }
-
-    // MARK: - Table View
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
-
+    
 }
+
+class autoClass {
+    var velocidad : Velocidades          //Coregir lo de iniciar como una instancia de velocidades
+    init(velocidad : Velocidades) {
+        self.velocidad = velocidad
+    }
+}
+
+
+func cambioVelocidad(VelActual actual : Int, velocidadEnCadena : String) -> (Int, String) {
+    
+    let velocidadParametro = actual
+    var resultado : (Int, String)
+    
+    
+    if velocidadParametro == 0 {
+        resultado = (20, "Velocidad Baja")
+    }
+    else if velocidadParametro == 20 {
+        resultado = (50, "Velocidad Media")
+    }
+    else if velocidadParametro == 50 {
+        resultado = (120, "Velocidad Alta")
+    }
+    else {
+        resultado = (50, "Velocidad Media")
+    }
+    return resultado
+}
+
+var auto = autoClass(velocidad: Velocidades.Apagado)
+print ("\(auto.velocidad.rawValue) , Apagado")
+
+for index in 1...20 {
+    let (velPrint, mensajePrint) =    cambioVelocidad(VelActual: auto.velocidad.rawValue,velocidadEnCadena: "")
+    if velPrint == 0 {
+        auto.velocidad = Velocidades.Apagado
+    } else if velPrint == 20 {
+        auto.velocidad = Velocidades.VelocidadBaja
+    } else if velPrint == 50 {
+        auto.velocidad = Velocidades.VelocidadMedia
+        
+    } else if velPrint == 120 {
+        auto.velocidad = Velocidades.VelocidadAlta
+    }
+    
+    
+    print ("\(velPrint) , \(mensajePrint)")
+    //print (auto.velocidad)
+}
+
+
 
